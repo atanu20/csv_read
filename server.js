71f7c3decf01f445a4562c3241c71063404d05d3
atanu <atanuj383@gmail.com>
2022-08-27 21:05:06 +0530

    add

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
   // // const data = xlsx.utils.sheet_to_json(ws);
   // // console.log(data);
 
-  res.send('Welcome to Bill Management');
+  res.send('Welcome to Data Management');
 });
 
 // Connect to mongodb
